969241b787ee6db7e7aa86f72d1f1d1be289714c
Andres <andres.mm07@gmail.com>
2015-11-05 01:16:36 -0800

    finished refactoring to mongodb

--- app/config.js
+++ app/config.js
@@ -1,41 +1,21 @@
 var Bookshelf = require('bookshelf');
 var mongoose = require('mongoose');
 var path = require('path');
+var crypto = require('crypto');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
 var host = process.env.host || '127.0.0.1';
 
-//mongo URI: mongodb://MongoLab-c:cnc8Hxo5LglLCo3s5fJbbI_.8CRjQqZSa08klgCzIok-@ds048878.mongolab.com:48878/MongoLab-c
-
-// var db = Bookshelf.initialize({
-//   client: 'sqlite3',
-//   connection: {
-//     host: host,
-//     user: 'your_database_user',
-//     password: 'password',
-//     database: 'shortlydb',
-//     charset: 'utf8',
-//     filename: path.join(__dirname, '../db/shortly.sqlite')
-//   }
-// });
-
 var Schema = mongoose.Schema;
-mongoose.connect('mongodb://localhost/shortly');
 
-var User;
-var Url;
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
   var urlSchema = new Schema({
-      url  :  { type: String },
-      base_url   :  { type: String },
-      code   :  { type: String},
-      title : { type: String},
-      visits : { type: Number },
-      date  :  { type: Date, default: Date.now }
+      url:  { type: String },
+      base_url:  { type: String },
+      code:  { type: String},
+      title: { type: String},
+      visits: { type: Number },
+      date :  { type: Date, default: Date.now }
   });
 
   var userSchema = new Schema({
@@ -44,78 +24,27 @@ db.once('open', function (callback) {
     date  :  { type: Date, default: Date.now }
   });  
 
-
-  User = mongoose.model('User', userSchema);
-  Url = mongoose.model('Url', urlSchema);
-
-  
-  
-});
-
-userSchema.methods.comparePassword = function(attemptedPassword, callback) {
-  bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-    callback(isMatch);
-  });
-};
-
-userSchema.methods.hashPassword = function() {
-  var cipher = Promise.promisify(bcrypt.hash);
-  return cipher(this.password, null, null).bind(this)
-    .then(function(hash) {
-      this.password = hash;
+  userSchema.methods.comparePassword = function(attemptedPassword, callback) {
+    bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+      callback(isMatch);
     });
-};
+  };
 
-urlSchema.methods.initialize = function(){
-    this.on('creating', function(model, attrs, options){
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-};
+  userSchema.methods.hashPassword = function() {
+    var cipher = Promise.promisify(bcrypt.hash);
+    return cipher(this.password, null, null).bind(this)
+      .then(function(hash) {
+        this.password = hash;
+      });
+  };
 
-//   comparePassword: function(attemptedPassword, callback) {
-// bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-//   callback(isMatch);
-// });
-//   },
-//   hashPassword: function(){
-    // var cipher = Promise.promisify(bcrypt.hash);
-    // return cipher(this.get('password'), null, null).bind(this)
-    //   .then(function(hash) {
-    //     this.set('password', hash);
-    //   });
-//   }
+  urlSchema.methods.initialize = function(){
+    var shasum = crypto.createHash('sha1');
+    shasum.update(this.url);
+    this.code = shasum.digest('hex').slice(0, 5);
+  };
 
-// db.knex.schema.hasTable('urls').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('urls', function (link) {
-//       link.increments('id').primary();
-//       link.string('url', 255);
-//       link.string('base_url', 255);
-//       link.string('code', 100);
-//       link.string('title', 255);
-//       link.integer('visits');
-//       link.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 100).unique();
-//       user.string('password', 100);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-
-module.exports = {User: User, Url:Url};
+var User = mongoose.model('User', userSchema);
+var Url = mongoose.model('Url', urlSchema); 
+mongoose.connect('mongodb://127.0.0.1/shortly');
+module.exports = { User: User, Url: Url };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('./server-config.js');
 
 app.set('port',  process.env.PORT || 4568);
-
+//app.listen(4568);
 app.listen(app.get('port'));
 
 console.log('Server now listening on port ' + app.get('port'));
